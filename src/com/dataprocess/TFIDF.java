6e7e23cfb21da3ff43d2c3f54943f6cdd87776ce
qqdown <ysht1993@163.com>
2015-10-08 10:55:19 +0800

    改正了IDF的计算

--- src/com/dataprocess/TFIDF.java
+++ src/com/dataprocess/TFIDF.java
@@ -1,12 +1,8 @@
 package com.dataprocess;
 
 import java.util.ArrayList;
-import java.util.Dictionary;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
 
 public class TFIDF
@@ -54,13 +50,13 @@ public class TFIDF
 	public static Set<TWord> TCalculate(List<TFile> fileList)
 	{
 		List<HashMap<TWord, Integer>> TCMapList = new ArrayList<HashMap<TWord, Integer>>();
-		HashMap<TWord, List<Integer>> TDMap = new HashMap<TWord, List<Integer>>(); 
+		HashMap<TWord, List<Integer>> IDFMap = new HashMap<TWord, List<Integer>>(); 
 		for(int i=0; i<fileList.size(); i++)
 		{
 			TFile tfile = fileList.get(i);
 			//获取TD中的TC
 			HashMap<TWord, Integer> TCMap = new HashMap<TWord, Integer>();
-
+			
 			for(TWord w : tfile.getWords())
 			{
 				if(TCMap.containsKey(w))
@@ -69,27 +65,28 @@ public class TFIDF
 					TCMap.put(w, count+1);
 				}
 				else
+				{
 					TCMap.put(w, 1);
-					
-				//记录IDF中每个Term出现在文件中的个数
-				if(TDMap.containsKey(w))
-				{
-					List<Integer> list = TDMap.get(w);
-					if(!list.contains(i))
+					//同一个词可能在一个文件中出现多次，但是在计算idf时，同一个文件不能重复计算多次
+					//记录IDF中每个Term出现在文件中的个数
+					if(IDFMap.containsKey(w))
 					{
-						list.add(i);
-						TDMap.put(w, list);
+						List<Integer> list = IDFMap.get(w);
+						if(!list.contains(i))
+						{
+							list.add(i);
+							IDFMap.put(w, list);
+						}
+							
 					}
-						
+					else
+					{
+						List<Integer> list = new ArrayList<Integer>();
+						list.add(i);
+						IDFMap.put(w, list);
+					}
 				}
-				else
-				{
-					List<Integer> list = new ArrayList<Integer>();
-					list.add(i);
-					TDMap.put(w, list);
-				}
-			}
-			
+			}	
 			TCMapList.add(TCMap);
 		}
 
@@ -102,12 +99,12 @@ public class TFIDF
 			{
 				Integer tc = TCMap.get(word);
 				double TF = tc*1.0;
-				double IDF = Math.log10(fileList.size()*1.0/TDMap.get(word).size());//计算IDF
+				double IDF = Math.log10(fileList.size()*1.0/IDFMap.get(word).size());//计算IDF
 				word.TFIDF = TF*IDF;
 			}
 		}
 		
-		return TDMap.keySet();
+		return IDFMap.keySet();
 	}
 }
 
